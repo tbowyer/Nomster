319861e85ebbb0fc763bd5de611e647d7bd4c29b
Trevor Bowyer <bowyer@gmail.com>
2015-10-17 17:07:57 +0000

    Added functionality to add place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,20 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.order(:name).page(params[:page]).per(5)
+		@places = Place.order(:id).page(params[:page]).per(5)
 	end
 
 	def new
 		@place = Place.new
+	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
 	end
 end
