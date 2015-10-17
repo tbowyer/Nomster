e5c6dba72e0070ed01126d46db842f4710d3a9db
Trevor Bowyer <bowyer@gmail.com>
2015-10-17 18:16:13 +0000

    installed devise gem and set up user model

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'kaminari'
 
+gem 'devise', '>= 3.2.4'
+
 gem 'bootstrap-sass', '~> 3.3.5.1'
 
 gem 'sprockets', '2.11.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Nomster::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'nomster-trevor-bowyer.herokuapp.com' }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
