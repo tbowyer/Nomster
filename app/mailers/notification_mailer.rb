1f27c6ed397cf0496ff1067bca086e0cb5f7bec8
Trevor Bowyer <bowyer@gmail.com>
2015-10-24 16:14:12 +0000

    hard coded email sending to see if works

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@wingsterapp.com"
 
   def comment_added
-  	mail(to: "bowyer@gmail.com",
+  	mail(to: "wingster45@gmail.com",
   		subject: "A comment has been added to your place")
   end
 end
